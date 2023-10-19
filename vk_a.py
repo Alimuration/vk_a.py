5a3c456f2b8992e64417691429e6f881b11e89ca
Alimurat <125111539+Alimuration@users.noreply.github.com>
2023-10-19 18:21:53 +0500

    justfun
    IT WORKS

--- vk_a.py
+++ vk_a.py
@@ -1,18 +1,20 @@
 import requests
 import vk_api
+from vk_api.utils import get_random_id
 
 session = requests.Session()
-login, password = 'login', 'password'
-vk_session = vk_api.VkApi(login, password)
-
+vk = vk_api.VkApi(login='login', password='password', app_id=2685278)
+vk.auth()
 try:
-    vk_session.auth(token_only=True)
+    vk.auth(token_only=True)
 except vk_api.AuthError as error_msg:
     print(error_msg)
+
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-longpoll = VkLongPoll(vk_session)
-vk = vk_session.get_api()
+
+longpoll = VkLongPoll(vk)
+vk = vk.get_api()
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
         # Слушаем longpoll, если пришло сообщение то:
@@ -20,10 +22,11 @@ for event in longpoll.listen():
             if event.from_user:  # Если написали в ЛС
                 vk.messages.send(  # Отправляем сообщение
                     user_id=event.user_id,
-                    message='нет'
+                    message='нет (от бота)',
+                    random_id=get_random_id()
                 )
-            elif event.from_chat:  # Если написали в Беседе
+            elif event.from_chat: # Если написали в Беседе
                 vk.messages.send(  # Отправляем собщение
                     chat_id=event.chat_id,
-                    message='пизда'
-                )
+                    message='NO'
+            )
